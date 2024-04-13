f8adba17d937ad1f739a8514b9d1a78dc0bccae5
meysamansari <143381707+meysamansari@users.noreply.github.com>
2024-04-13 04:20:27 +0330

    Add migrations for creating categories and tasks tables

---
+++ database/migrations/2024_04_12_221449_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longtext('description')->nullable();
+            $table->enum('status', ['todo', 'doing', 'done', 'cancelled'])->default('todo');
+            $table->date('due_date');
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
